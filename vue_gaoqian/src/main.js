1d2b9f9427204617fa51c15a1698651fee72a7f1
lishifu666 <1522120777@qq.com>
2022-07-28 17:57:37 +0800

    注册对话框完成3/1 页脚页头组件修改完成

--- vue_gaoqian/src/main.js
+++ vue_gaoqian/src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import HeadBar from './components/HeadBar'
+import FooterBar from './components/FooterBar'
 
 Vue.config.productionTip = false
+
+//全局注册 HeadBar 组件
+Vue.component("headBar", HeadBar)
+
+//全局注册 FooterBar 组件
+Vue.component("footerBar", FooterBar)
 
 new Vue({
   router,
--- vue_gaoqian/src/views/HomeView.vue
+++ vue_gaoqian/src/views/HomeView.vue
@@ -2,16 +2,8 @@
   <v-app>
     <!-- 应用栏区域 -->
     <v-app-bar app color="#ffffff">
-      <!-- 应用栏容器 -->
-        <v-container fluid>          
-        <!-- 应用栏菜单选项 -->
-          <v-tabs centered slider-size="5">
-            <v-tab v-for="tab in tabMenus" :key="tab.id" :to="tab.href">{{tab.name}}</v-tab>
-          </v-tabs>
-          <!-- 登录按钮 -->
-          <v-btn absolute right top text class="mr-16" >登录</v-btn>
-          <login-register-dialog></login-register-dialog>
-        </v-container>
+      <!-- 使用 HeadBar 组件 -->
+      <head-bar></head-bar>
     </v-app-bar>
     <v-main>
       <!-- 视差背景图 -->
@@ -42,9 +34,8 @@
       </v-container>
     </v-main>
     <v-footer class="text-center">
-      <v-container>
-        <v-chip small label color="transparent" v-for="link in footerLinks" :to="link.to" :key="link.id">{{link.text}}</v-chip>
-      </v-container>
+      <!-- 使用 FooterBar 组件 -->
+      <footer-bar></footer-bar>
     </v-footer>
   </v-app>
 </template>
@@ -56,23 +47,11 @@
     name: 'Home',
     components: {LoginRegisterDialog},
     data: () => ({
-      tabMenus: [],  //应用栏菜单
+      
       homeBackPic: '',  //首页背景图
       homeCards: [],  //主页卡片信息
-      footerLinks: [],  //页脚链接
     }),
     methods: {
-      //获取应用栏菜单
-      getAppBarMenus (){
-        //请求服务器 -- 获取 --应用栏菜单
-        this.tabMenus = [
-          {id: 1, name: '首页', href: '/'},
-          {id: 2, name: '博客', href: '/blog'},
-          {id: 3, name: '考证', href: '/exam'},
-          {id: 4, name: '影音', href: '/film'},
-          {id: 5, name: '学堂', href: '/school'},
-        ]
-      },
       //获取首页的背景图
       getHomeBackPic() {
         //请求服务器 -- 获取首页的背景图地址
@@ -155,32 +134,13 @@
             ]
           }, 
         ]
-      },
-      //获取页脚信息
-      getFooterLinks() {
-        //请求服务器 -- 获取页脚链接
-        this.footerLinks = [
-          {id: 1, text: '增值电信业务经营许可证： 合字B2-2009007', to: ''},
-          {id: 2, text: '京ICP备10036305号-7', to: ''},
-          {id: 3, text: '京公安网备11010802022657', to: ''},
-          {id: 4, text: '隐私声明和 Cookie', to: ''},
-          {id: 5, text: '法律声明', to: ''},
-          {id: 6, text: '广告', to: ''},
-          {id: 7, text: '帮助', to: ''},
-          {id: 8, text: '反馈', to: ''},
-          {id: 9, text: '© 2022 Microsoft', to: ''},
-        ]
       }
     },
       created() {
-        //获取应用栏菜单
-        this.getAppBarMenus()
         //获取首页背景图
         this.getHomeBackPic()
         //获取主页卡片信息
         this.getHomeCards()
-        //获取页脚链接
-        this.getFooterLinks()
       }
   }
 </script>
